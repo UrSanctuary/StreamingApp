5235c50cf05daca682b3423095cf6cc2b41385db
UrSanctuary <64437373+UrSanctuary@users.noreply.github.com>
2021-04-30 11:12:33 +0300

    Initial version
    Not tested according to incompatibility of scala 2.12 and spark 3.1.1.
    The version of spark should be changed to 2.4.7

---
+++ Streaming.scala
@@ -0,0 +1,40 @@
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.SparkSession
+import java.sql.Timestamp
+
+class Srteaming {
+    def main(args: Array[String]): Unit = {
+    if (args.length < 3) {
+      System.err.println("Usage: StructuredSessionization <FileName> <time_field_name> <time_period>")
+      System.exit(1)
+    }
+    val FileName = args(0)
+    val time_field_name = args(1)
+    val time_period = args(2)
+//create session 
+val spark = SparkSession
+  .builder
+  .appName("StreamingApp")
+  .getOrCreate()  
+ 
+ spark.sparkContext.setLogLevel("ERROR")
+
+//get .parquet file
+val df  = spark
+  .readStream
+  .option("sep", ",")     
+  .parquet(FileName)  //<-- an error may occur here
+//process somehow data
+//------------------------------------------------------------
+//select data by time_field_name
+  df.select(time_field_name).show()
+//------------------------------------------------------------
+//set window time
+val windowTimeSelect = df.groupBy(time_period)
+
+//print result to the console
+ 
+import spark.implicits._
+  }
+
+}
